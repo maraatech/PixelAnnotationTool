8832313f376837a95d7e8c069881348891f8ce4d
Trevor Gee <trevorgeek@gmail.com>
2019-09-20 10:43:16 +1200

    Improves linux build process

--- scripts_to_build/build_linux.sh
+++ scripts_to_build/build_linux.sh
@@ -5,7 +5,10 @@ if [ -d "build" ]; then
 fi
 mkdir build
 cd build
-cmake -DQT5_DIR=/home/amaury/Qt5.9.1/5.9.1/gcc_64/lib/cmake -G"Unix Makefiles" ../..
+cmake -DQT5_DIR=/home/anyone/Qt5.9.1/5.9.1/gcc_64/lib/cmake -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 -G"Unix Makefiles" ../..
 make
 cd ..
-
+if [ -d "../build" ]; then
+	rm -r ../build
+fi
+mv build ../build
