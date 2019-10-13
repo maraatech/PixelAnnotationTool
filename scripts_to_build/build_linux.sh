1a37ae0d21ae71571de5ce1674ac2d19ff91c8e7
PastaTime <arran94@gmail.com>
2019-10-14 10:43:55 +1300

    Add clarifying comments

--- scripts_to_build/build_linux.sh
+++ scripts_to_build/build_linux.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+#
+## IMPORTANT:
+# Before running this script please fill in the required fields in CMakeLists.txt
 
 if [ -d "build" ]; then
 	rm -r build
 fi
 mkdir build
 cd build
-cmake -DQT5_DIR=/home/anyone/Qt5.9.1/5.9.1/gcc_64/lib/cmake -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 -G"Unix Makefiles" ../..
+cmake -G"Unix Makefiles" ../..
 make
 cd ..
 if [ -d "../build" ]; then
