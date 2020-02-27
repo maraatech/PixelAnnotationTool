7cfd8850636a780e145bb6e36d0a4ba1bc54c1b9
Arran Davis <arran94@gmail.com>
2020-02-27 18:02:56 +1300

    Update CMake to support Windows build

--- scripts_to_build/win_make_vc14_x64_to_edit.bat
+++ scripts_to_build/win_make_vc14_x64_to_edit.bat
@@ -3,7 +3,9 @@ cd build
 mkdir x64
 cd x64
 
-cmake -DQT5_DIR=/path/to/Qt/5.9.1/msvc2015_64/lib/cmake -DCMAKE_PREFIX_PATH=/path/to/OpenCV/build -G "Visual Studio 14 Win64" ../../..
+rem "Before running this script please make sure the appropriate paths are set in CMakeLists.txt"
+
+cmake -G "Visual Studio 14 Win64" ../../..
 
 cmake --build . --config Release
 
